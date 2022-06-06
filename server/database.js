c24db96f448642926d42f93e04e39c7d36a3ac4b
jingwei950 <jingwei950@hotmail.sg>
2022-06-06 15:23:05 +0800

    Deleted server folder

--- server/database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require("mysql2");
-
-module.exports = mysql.createConnection({
-    host: 'localhost',
-    user: '34053405',
-    password: '34053405',
-    database: 'TestApp'
-  });
